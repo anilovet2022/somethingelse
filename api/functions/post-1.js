e2d8e9103c5f9b9e9ab9a6c19310601bb1656716
Ermal <63654158+lamre96@users.noreply.github.com>
2022-05-17 16:00:05 +0200

    Initial commit

---
+++ api/functions/post-1.js
@@ -0,0 +1,20 @@
+exports.handler = async event => {
+  if (event.queryStringParameters.fbclid) {
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: 'https://theancientzen.com/humans-are-aliens-according-to-an-expert-and-we-were-brought-to-earth-hundreds-of-thousands-of-years-ago/'
+      }
+    }
+  } else {
+    let pathName = event.path.split('/')[3].split('-')
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: process.env.URL + '/' + pathName[0] + '-' + pathName[1] + '/'
+      }
+    }
+  }
+}
